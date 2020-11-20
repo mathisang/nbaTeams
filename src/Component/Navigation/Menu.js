14e37ceafff25d62e5e282ef266d8af023849ff1
Angibaud Mathis <m.angibaudmathis@gmail.com>
2020-11-20 12:22:10 +0100

    Animations Searchbox & Icon menu

---
+++ src/Component/Navigation/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Menu() {
+    return (
+        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 3C0 1.34315 1.34315 0 3 0H8V8H0V3Z" fill="white"/>
+            <rect y="11" width="8" height="8" fill="white"/>
+            <path d="M3 30C1.34315 30 -5.87108e-08 28.6569 -1.31134e-07 27L-3.49691e-07 22L8 22L8 30L3 30Z" fill="white"/>
+            <path d="M11 0H19V8H11V0Z" fill="white"/>
+            <rect x="11" y="11" width="8" height="8" fill="white"/>
+            <rect x="11" y="22" width="8" height="8" fill="white"/>
+            <path d="M27 -1.31134e-07C28.6569 -5.87108e-08 30 1.34315 30 3L30 8L22 8L22 -3.49691e-07L27 -1.31134e-07Z" fill="white"/>
+            <rect x="22" y="11" width="8" height="8" fill="white"/>
+            <path d="M30 27C30 28.6569 28.6569 30 27 30L22 30L22 22L30 22L30 27Z" fill="white"/>
+        </svg>
+    );
+}
+
+export default Menu;
--- src/Component/Navigation/Navigation.js
+++ src/Component/Navigation/Navigation.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Logo from "./Logo";
 import Search from "../Navigation/Search";
+import Menu from "../Navigation/Menu";
 
 function Navigation() {
     return (
         <div className="navigation">
-            <Logo />
-            <Search />
+            <div className="leftSide">
+                <Logo />
+                <Search />
+            </div>
+            <div className="rightSide">
+                <Menu />
+            </div>
         </div>
     );
 }
