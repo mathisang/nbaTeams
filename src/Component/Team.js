5769c4b31deee3191c5d458008d4856ee1a40f1e
Angibaud Mathis <m.angibaudmathis@gmail.com>
2020-12-15 15:07:02 +0100

    Atlanta to Lakers

--- src/Component/Team.js
+++ src/Component/Team.js
@@ -4,8 +4,10 @@ import ThemeSwitcher from 'react-css-vars';
 import Tilt from 'react-parallax-tilt';
 
 const themeTeamBox = {
-    colorButton: '#F37178',
-    hoverButton: '#f3999d',
+    // colorButton: '#F37178',
+    // hoverButton: '#f3999d',
+    colorButton: '#E2A106',
+    hoverButton: '#ecb01c',
     teamUrl: 'url("/images/teams/atlanta/background.jpg")'
 }
 
@@ -19,8 +21,8 @@ function Team() {
                     <Tilt className="Tilt" trackOnWindow={true} tiltMaxAngleX="8" tiltMaxAngleY="8" transitionSpeed="400" transitionEasing="cubic-bezier(0,.75,.77,.73)" tiltReverse={true} reset={false}>
                         <div className="content">
                             <div className="Tilt-inner">
-                                <h1>Atlanta<br/>
-                                    <span>HAWKS</span></h1>
+                                <h1>Los Angeles<br/>
+                                    <span>LAKERS</span></h1>
 
                                 <button>
                                     Voir l'équipe
